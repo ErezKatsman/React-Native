7ad6aaa6d1841bd4de9f835f5058cadc62f15f43
ErezKatsman <erezkatsman2311@gmail.com>
2021-08-08 00:08:17 +0300

    one component todo

--- todolist/App.js
+++ todolist/App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const inputHandler = (text) => {
+    console.log(text);
+    setInput(text);
+  };
+
+  const addHnadler = () => {
+    setTodos((prevTodos) => [...prevTodos, input]);
+    setInput("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        value={input}
+        placeholder="Enter The Goal"
+        onChangeText={inputHandler}
+      />
+      <Button title="Add Goal" onPress={addHnadler} />
+      {todos.map((todo, i) => (
+        <Text key={i} t>
+          {todo}
+        </Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
